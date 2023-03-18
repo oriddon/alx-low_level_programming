3c9609f6d0192f3c7c83293e6bb70e709dd29c29
Athanasius Alekwe <oriddon@gmail.com>
2023-03-18 09:46:07 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Description: A C program that prints with printf function
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
+	int i;
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
-
-return (0);
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
 }
