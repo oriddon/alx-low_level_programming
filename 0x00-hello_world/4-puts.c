1bbc0208bc67a4826485153b042d98000f6da39b
Athanasius Alekwe <oriddon@gmail.com>
2023-03-18 06:36:53 -0700

    commiting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - The entry point
+* Description: C program printing with put function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
