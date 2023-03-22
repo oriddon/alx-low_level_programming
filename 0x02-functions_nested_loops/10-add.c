d63ecc7fb5eb339c873a52bfa31e47b2fcb9a4b4
Athanasius Alekwe <oriddon@gmail.com>
2023-03-22 16:25:41 -0700

    commitng

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * add - addition numbers
+ * @a: first number
+ * @b: second number
+ * Return: always 0
+*/
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
