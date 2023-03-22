7472b027c8ca62551d310cb85a96780f7ba68bb7
Athanasius Alekwe <oriddon@gmail.com>
2023-03-22 16:29:06 -0700

    commiting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,44 @@
-#include "holberton.h"
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
 /**
-* times_table - that prints the 9 times table
-*
-* Return: no return
+ * times_table - printing table with number
 */
+
 void times_table(void)
 {
-	int a, b, c;
-
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar(48);
-		for (b = 1; b <= 9; b++)
-		{
-			c = a * b;
-			_putchar(44);
-			_putchar(32);
-			if (c <= 9)
-			{
-				_putchar(32);
-				_putchar(c + 48);
-			}
-			else
-			{
-				_putchar((c / 10) + 48);
-				_putchar((c % 10) + 48);
-			}
-		}
-	_putchar('\n');
-	}
+int i, j, mult, pre;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+mult = j * i;
+if (mult <= 9)
+{
+printf("%d", mult);
+if (j != 9)
+{
+pre = i * (j + 1);
+if (pre <= 9)
+{
+printf(",  ");
+}
+else
+{
+printf(", ");
+}
+}
+}
+else
+{
+printf("%d", mult);
+if (j != 9)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+}
 }
