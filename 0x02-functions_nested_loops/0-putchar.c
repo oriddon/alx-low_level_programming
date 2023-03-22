ba70d51c41bda1b35e02a40dcb01a1c1564f8ac3
Athanasius Alekwe <oriddon@gmail.com>
2023-03-22 08:58:36 -0700

    adding -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - including the main.h
+ * description - showing how an main.h can be include
+ * return 0 
+ */
+int main(void)
+{ 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+}
