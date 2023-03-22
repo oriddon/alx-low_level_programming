ebdac4a255f65a0a5a121dd4901b3934f8428b13
Athanasius Alekwe <oriddon@gmail.com>
2023-03-22 16:15:21 -0700

    commiting

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_times_table - printing table with number
+ * @n: number of loop
+*/
+void print_times_table(int n)
+{
+if (n <= 15 && n >= 0)
+{
+int i, j, mult, pre;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+mult = j * i;
+if (mult <= 9)
+{
+printf("%d", mult);
+if (j != n)
+{
+pre = i * (j + 1);
+if (pre <= 9)
+printf(",   ");
+else
+printf(",  ");
+}
+}
+else if (mult <= 99)
+{
+printf("%d", mult);
+if (j != n)
+{
+pre = i * (j + 1);
+if (pre <= 99)
+printf(",  ");
+else
+printf(", "); }
+}
+else
+{
+printf("%d", mult);
+if (j != n)
+printf(", "); }
+}
+printf("\n");
+}
+}
+}
