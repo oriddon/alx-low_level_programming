ad633eca2375506fca0576760efbe7627174ac19
Athanasius Alekwe <oriddon@gmail.com>
2023-04-11 08:10:11 -0700

    commiting

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,10 +1,16 @@
 #include "main.h"
+#include <ctype.h>
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
- */
+ * _isdigit - check number is a digit
+ * description: checks the number
+ * @c: first parameter
+ * Return: return 1 if upper otherwise 0
+*/
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	return (0);
 }
